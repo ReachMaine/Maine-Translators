ab1647e16f5bf3b188f679787ccdf19b3a047454
Linda Speight <linda@reachmaine.com>
2017-01-18 10:41:37 -0500

    site maint
    add favicon,  GTM code, remove maine hosting in footer.

--- site/index.php
+++ site/index.php
@@ -1,7 +1,7 @@
 <?php
-function obfuscate ($str) { 
-     $return = ''; 
-     for($i = 0; $i < strlen($str); $i++) 
+function obfuscate ($str) {
+     $return = '';
+     for($i = 0; $i < strlen($str); $i++)
           $return .= '&#' . (rand(0,1) == 1 ? hexdec(bin2hex(substr($str, $i, 1))) : 'x' . bin2hex(substr($str, $i, 1))) . ';';
      return $return;
 }
@@ -40,17 +40,26 @@ $page = $path[0];
 <script type="text/javascript" src="js/niftycube.js"></script>
 <script type="text/javascript" src="js/style.js"></script>
 <script type="text/javascript" src="js/sifr.js"></script>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
 </head>
 <body>
 
 <div id="body">
-
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TLWNXT"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TLWNXT');</script>
+    <!-- End Google Tag Manager -->
 	<div id="page">
 
 		<div id="header">
 			<h1><a href="" class="logo"><span>The Association of Maine Interpreters &amp; Translators</span></a></h1>
 		</div>
-		
+
 		<div id="nav">
 			<ul>
 				<li><a href="/">Home</a></li>
@@ -62,7 +71,7 @@ $page = $path[0];
 				<li class="last"><a href="contact/">Contact Us</a></li>
 			</ul>
 		</div>
-		
+
 		<div id="content">
 		<?php
 		if (!empty($page) && file_exists($page . '.php')) {
@@ -73,15 +82,15 @@ $page = $path[0];
 		}
 		?>
 		</div>
-		
+
 		<div id="footer">
 			<p>&copy; <?=date('Y');?> Association of Maine Interpreters &amp; Translators</p>
 			<p><?php /*echo implode('', file('http://www.avertismedia.com/credit.php?site=amit'));*/ ?></p>
-			<p><?php echo "Website hosted and maintained by <a href='http://www.mainesoft.com'>Maine Software & Internet Solutions</a>"; ?></p>
+
 		</div>
 
 	</div>
-	
+
 </div>
 
 </body>
