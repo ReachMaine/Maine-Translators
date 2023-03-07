83a2072ca6b9ac6875b23a534621eb6a7e874843
thanisorn <102362521+ramenshoyu@users.noreply.github.com>
2023-03-07 10:52:10 -0500

    Update find-interpreter.php
    +Update 2023 Membership .pdf
    +file is bios2023.pdf

--- site/find-interpreter.php
+++ site/find-interpreter.php
@@ -1,5 +1,5 @@
 <h2>Find an Interpreter or Translator</h2>
 
-<p><a href="bios.pdf">Member List</a></p>
+<p><a href="bios2023.pdf">Member List</a></p>
 
 <p>Member information is currently available only in PDF format.</p>
